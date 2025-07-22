9c0d9e1f0e34acc75850076950e5ed89a4cfd50f
Antima <antima142005@gmail.com>
2025-07-22 08:37:15 +0000

    apply above code again

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,10 @@
-
 "use client";
 
-import BlogClient from "./BlogClient";
 import { Suspense } from "react";
+import BlogClient from "./BlogClient";
 
 export default function BlogPage() {
+  // Data fetching is now done in BlogClient
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <BlogClient />
